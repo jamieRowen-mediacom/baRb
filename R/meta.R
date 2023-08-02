eaeeca609b2598a269f33b5ccd360490eb1c13d2
jamieRowen-mediacom <jamie.owen@mediacom.com>
2023-08-02 07:29:23 +0100

    docs: add function documentation for the meta endpoints

--- R/meta.R
+++ R/meta.R
@@ -1,14 +1,11 @@
-ensure_no_slash = function(x) {
-  x %>%
-    stringr::str_remove("^/") %>%
-    stringr::str_remove("/$")
-}
-
+#' @include utils.R
 meta_class_name_ = function(x) {
   x = ensure_no_slash(x)
   glue::glue("barb_meta_{x}")
 }
 
+#' @include rexport-tibble.R
+#' @include utils.R
 meta_gen_ = function(path, ..., class = meta_class_name_(path)) {
   force(class)
   path = ensure_slashes(path)
@@ -22,18 +19,116 @@ meta_gen_ = function(path, ..., class = meta_class_name_(path)) {
   })
 }
 
+#' Get the list of panels
+#'
+#' Returns a complete list of panel codes and panel names.
+#' This is useful for finding the correct panel code when using panel
+#' code as a query parameter for other endpoints.
+#'
+#' @param as_tibble if TRUE cast result as a tibble object, otherwise list
+#'   representation of the raw json response
+#' @inheritParams barb
+#' @export
+#' @examples \dontrun{
+#'   meta_panels()
+#' }
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_panels = meta_gen_("panels")
+
+#' Get the list of stations
+#'
+#' Informations about the stations codes is used as a
+#' query parameter in the advertising spots, audiences_by_time, 
+#' programme_ratings, spot_schedule, programme_schedule endpoints.
+#'
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_stations = meta_gen_("stations")
+
+#' Get the list of advertisers
+#' 
+#' Returns a complete list of advertiser names
+#' 
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_advertisers = meta_gen_("advertisers")
+
+#' Get the list of buyers
+#' 
+#' Returns a complete list of buyer names.
+#' 
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_buyers = meta_gen_("buyers")
+
+#' Get the list of panel households
+#' 
+#' Returns a list of households, where each household is acombination of
+#' household data and a list of the devices present in the household.
+#' 
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_households = meta_gen_("households")
+
+#' Get the list of panel members
+#' 
+#' Returns details for a set of panel members.
+#' 
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_panel_members = meta_gen_("panel_members")
+
+#' Get the list of target audience categories
+#' 
+#' Returns the list of target audience categories by selected period and panel.
+#' 
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_target_audience_members = meta_gen_("target_audience_members")
+
+#' Get the list of viewing stations
+#' 
+#' Information about the viewing stations codes is used as a query parameter
+#' in the viewing, programme_audience endpoints.
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_viewing_stations = meta_gen_("viewing_stations")
+
+#' Get the spots scheduled
+#' 
+#' Get all the spots scheduled by each station.
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_spot_schedule = meta_gen_("spot_schedule")
+
+#' Get the programmes scheduled
+#' 
+#' Get all the programmes scheduled by each station.
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_programme_schedule = meta_gen_("programme_schedule")
+
+#' Get the list of programme content details
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_programme_content_details = meta_gen_("programme_content_details")
+
+#' Get the list of transmission log programme details
+#' @inheritParams meta_panels
+#' @export
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 meta_transmission_log_programme_details = meta_gen_("transmission_log_programme_details")
+
 
 #' Get the list of panels
 #'
@@ -44,21 +139,16 @@ meta_transmission_log_programme_details = meta_gen_("transmission_log_programme_
 #' @param as_tibble if TRUE cast result as a tibble object, otherwise list
 #'   representation of the raw json response
 #' @inheritParams barb
-#' @include rexport-tibble.R
 #' @export
 #' @examples \dontrun{
-#'   barb_get_panels()
+#'   meta_panels()
 #' }
+#' @return if as_tibble, a tibble, otherwise list representation of raw json
 barb_get_panels = function(as_tibble = TRUE, token = NA_character_) {
-  res = barb("/panels", token = token) %>%
-    with_class("barb_panels")
-  if (as_tibble) {
-    res = as_tibble(res)
-  }
-  res
+  meta_panels(as_tibble = as_tibble, token = token)
 }
 
-#' @s3method as_tibble barb_panels
+#' @exportS3Method as_tibble barb_panels
 #' @rdname as_tibble
 #' @export
 as_tibble.barb_panels = function(
